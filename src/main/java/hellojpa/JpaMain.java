5f42af102c3eb14017c183b0abcf558a4d402b4f
Chanbeom <kin280000@gmail.com>
2022-03-18 11:40:17 +0900

    20220318v1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -18,12 +17,12 @@ public class JpaMain {
 
         try {
 
-            //영속
-            Member findMember1 = em.find(Member.class, 100L);
-            Member findMember2 = em.find(Member.class, 100L);
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.ADMIN);
 
-            System.out.println("result = " + (findMember1 == findMember2));
-
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
