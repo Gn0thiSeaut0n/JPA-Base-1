5467ddf8fc65939d6102899edfb44b741ccf8701
Gn0thiseaut0n <kin280000@gmail.com>
2022-03-20 16:06:30 +0900

    20220320

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,21 +3,29 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ",
-        initialValue = 1, allocationSize = 1)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member() {
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 
     public Long getId() {
@@ -35,4 +43,12 @@ public class Member {
     public void setUsername(String username) {
         this.username = username;
     }
+
+//    public Long getTeamId() {
+//        return teamId;
+//    }
+
+//    public void setTeamId(Long teamId) {
+//        this.teamId = teamId;
+//    }
 }
