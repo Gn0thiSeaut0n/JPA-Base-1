8d011c98b562e8fa8c98c6033e3fbbe4022c9187
Chanbeom <kin280000@gmail.com>
2022-03-22 16:41:18 +0900

    Basev220220322

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
+import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -17,28 +17,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            //저장
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-
             Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
+            member.setUsername("user1");
+            member.setCreateBy("kim");
+            member.setCreateDate(LocalDateTime.now());
 
-            team.addMember(member);
+            em.persist(member);
 
             em.flush();
             em.clear();
-
-            Team findTeam = em.find(Team.class, team.getId());
-            List<Member> members = findTeam.getMembers();
-
-            for (Member m : members) {
-                System.out.println("m = " + findTeam);
-            }
-
 
             tx.commit();
         } catch (Exception e) {
